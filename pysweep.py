7c3a5f3928c7543454f631b0b4ece0f9a8d748c7
Nathanael Cox <nathanaeltcox@gmail.com>
2018-11-22 14:09:19 -1000

    Second commit, got the timer working.

--- pysweep.py
+++ pysweep.py
@@ -3,11 +3,11 @@ import subprocess
 import time
 
 def main():
-    start = time.process_time()
+    start = time.time()
     ip_target = "10.0.2."
-    ip_result = []
-    ip_response = []
-    ip_list = []
+    ip_result = [] #Stores the active IP addresses that are returned.
+    ip_response = [] #Stores the time for each ping.
+    ip_list = [] #Stores the active IP addresses as a list to be printed.
     for ip in range(0,7):
         output = b""
         ip_call = ip_target + str(ip)
@@ -25,14 +25,13 @@ def main():
             ip_response = output[2]
             print(ip_result + " is detected online. Response time(s) were: " + ip_response)
             ip_list.append(ip_result)
-
-    end = time.process_time()
-    seconds = end - start
+    seconds = time.time() - start
+    seconds = seconds * 1000 #Convert to milliseconds.
     print("The following hosts were found to be online and responding to ping requests:")
     print("\n""Detected Hosts:")
     print("==============")
     print("\n".join(ip_list))
     print("\n""Total time to scan took: " + str(seconds) + "ms")
-
+    
 if __name__ == "__main__":
     main()
